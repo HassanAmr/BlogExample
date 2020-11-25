5403bdb3ff17939f88852f12193f79f7f84988c1
Hassan Gamaleldin <hassan.amr89@gmail.com>
2020-11-25 22:04:22 +0100

    Added Entities and DbContext and a controller

---
+++ BlogExample/Controllers/BlogsController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlogExample;
+using BlogExample.Data;
+
+namespace BlogExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly BlogsContext _context;
+
+        public BlogsController(BlogsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Blogs
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Blog>>> GetBlog()
+        {
+            return await _context.Blogs.ToListAsync();
+        }
+
+        // GET: api/Blogs/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Blog>> GetBlog(int id)
+        {
+            var blog = await _context.Blogs.FindAsync(id);
+
+            if (blog == null)
+            {
+                return NotFound();
+            }
+
+            return blog;
+        }
+
+        // POST: api/Blogs
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Blog>> PostBlog(Blog blog)
+        {
+            _context.Blogs.Add(blog);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBlog", new { id = blog.BlogId }, blog);
+        }
+    }
+}
---
+++ BlogExample/Data/BlogsContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BlogExample;
+
+namespace BlogExample.Data
+{
+    public class BlogsContext : DbContext
+    {
+        public BlogsContext (DbContextOptions<BlogsContext> options)
+            : base(options)
+        {
+        }
+
+
+        public DbSet<BlogExample.Blog> Blogs { get; set; }
+        public DbSet<BlogExample.Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Blog>()
+                .HasMany(b => b.Posts)
+                .WithOne();
+
+            modelBuilder.Entity<Blog>()
+                .Navigation(b => b.Posts)
+                    .UsePropertyAccessMode(PropertyAccessMode.Property);
+        }
+    }
+}
